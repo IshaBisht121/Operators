18fb1c35fd50f136132f5eae671150223cc67b24
IshaBisht <ishubisht2002@gmail.com>
2024-01-23 14:59:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// Operatoits= is used to perform  oprations between two  two operend .
+
+// Arthmetic operator=it is use to perform a mathemetical operations.
+// Arithmetic Operators:
+
+// Addition (+)
+// Subtraction (-)
+// Multiplication (*)
+// Division (/)
+// Modulus (%)
+// Exponentiation (** or ^)
+
+
+// Comparison Operators:
+
+// Equal to (==)
+// Not equal to (!= or <>)
+// Greater than (>)
+// Less than (<)
+// Greater than or equal to (>=)
+// Less than or equal to (<=)
+// Logical Operators:
+
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+
+// Assignment Operators:
+
+// Assignment (=)
+// Addition assignment (+=)
+// Subtraction assignment (-=)
+// Multiplication assignment (*=)
+// Division assignment (/=)
+
+
+// Bitwise Operators:
+
+// Bitwise AND (&)
+// Bitwise OR (|)
+// Bitwise XOR (^)
+// Bitwise NOT (~)
+// Left shift (<<)
+// Right shift (>>)
+
+
+// Unary Operators:
+
+// Unary plus (+)
+// Unary minus (-)
+// Increment (++)
+// Decrement (--)
+
+// practice questions
+
+let a=4;
+let b=5;
+let c=a**4;
+console.log(`the squre os a is ${c}`)
